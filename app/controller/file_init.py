57318c5d04469ab48baaa8e162b32c93a652810e
kk-gc <kk.gc.lp@gmail.com>
2023-11-24 16:38:23 +0000

    Rename FileTools class to FileInitModel class and separate into their directories

--- app/controller/file_init.py
+++ app/controller/file_init.py
@@ -1,14 +1,12 @@
 import datetime
 import hashlib
 import json
-from typing import Optional
-from pathlib import Path
 from app.model.vocabulary_data import VocabularyData
+from app.model.file_init import FileInitModel
 from app.view.file_init import FileInitView
 
 
 class FileInitController:
-
     LOGGING_ON_SCREEN = True
     LOGGING_TO_FILE = True
     LOGGING_FILEPATH = '../../logs/log_file_init.txt'
@@ -31,27 +29,27 @@ class FileInitController:
     def __init__(self):
         # checking STARTER
         self.starter_path = VocabularyData.STARTER
-        self.starter_results = FileTools(path=self.starter_path,
-                                         exist=True,
-                                         checksums=True,
-                                         )
+        self.starter_results = FileInitModel(path=self.starter_path,
+                                             exist=True,
+                                             checksums=True,
+                                             )
         self.starter_exist = self.starter_results.exist
         self.starter_checksum_ok = self.check_starter_checksum()
 
         # checking TOLEARN
         self.to_learn_path = VocabularyData.TOLEARN
-        self.to_learn_results = FileTools(path=self.to_learn_path,
-                                          exist=True,
-                                          )
+        self.to_learn_results = FileInitModel(path=self.to_learn_path,
+                                              exist=True,
+                                              )
         self.to_learn_exist = self.to_learn_results.exist
 
         # checking INHAND
         self.hand_path = VocabularyData.INHAND
-        self.hand_results = FileTools(path=self.hand_path,
-                                      exist=True,
-                                      json_length=True,
-                                      json_key=True,
-                                      )
+        self.hand_results = FileInitModel(path=self.hand_path,
+                                          exist=True,
+                                          json_length=True,
+                                          json_key=True,
+                                          )
         self.hand_exist = self.hand_results.exist
         self.hand_length = self.hand_results.json_length
         self.hand_stars_count_ok = self.check_hand_stars_count()
@@ -59,18 +57,18 @@ class FileInitController:
 
         # checking MASTERED
         self.mastered_path = VocabularyData.MASTERED
-        self.mastered_results = FileTools(path=self.mastered_path,
-                                          exist=True,
-                                          json_length=True,
-                                          )
+        self.mastered_results = FileInitModel(path=self.mastered_path,
+                                              exist=True,
+                                              json_length=True,
+                                              )
         self.mastered_exist = self.mastered_results.exist
 
         # checking SETTINGS
         self.settings_path = VocabularyData.SETTINGS
-        self.settings_results = FileTools(path=self.settings_path,
-                                          exist=True,
-                                          json_length=True,
-                                          )
+        self.settings_results = FileInitModel(path=self.settings_path,
+                                              exist=True,
+                                              json_length=True,
+                                              )
         self.settings_exist = self.settings_results.exist
 
         # checking first run
@@ -195,88 +193,5 @@ class FileInitController:
             f.write(f'{_results}\n')
 
 
-class FileTools:
-    ALGORITHMS = {'sha1', 'md5', 'sha256', 'sha512', 'asa'}
-
-    def __init__(self,
-                 path: Path | str,
-                 exist=False,
-                 checksums=False,
-                 json_length=False,
-                 json_key=False,
-                 ):
-
-        self.path = Path(path)
-        self.do_exist = exist
-        self.do_checksums = checksums
-        self.do_json_length = json_length
-        self.do_json_key = json_key
-
-        self.exist = None
-        if self.do_exist:
-            self.exist = self._exist()
-
-        self.checksums = None
-        if self.do_checksums:
-            self.checksums = self._checksums()
-
-        self.json_length = None
-        if self.do_json_length:
-            self.json_length = self._json_length()
-
-    def __repr__(self):
-        return f'{self.path.name}'
-
-    def __str__(self):
-        return f'{self.path.name}'
-
-    def _exist(self) -> bool:
-        """
-        Check self.path existence
-        :return: True | False
-        """
-        return self.path.exists()
-
-    def _checksums(self) -> Optional[dict[str]]:
-        """
-        Generate file checksums for all available algorithms from self.ALGORITHMS
-        returning dict with algo name as a key and checksum as a value
-        :return: dict[str] | None
-        """
-        _return = {}
-        if not self.exist:
-            return None
-
-        with open(self.path, 'rb') as f:
-            _data = f.read()
-
-        # generate file hashes described in self.ALGORITHMS if they available
-        for algorithm in self.ALGORITHMS & hashlib.algorithms_available:
-            _return[algorithm] = hashlib.new(algorithm, _data).hexdigest()
-        return _return
-
-    def _json_length(self) -> Optional[int]:
-        """
-        Return json data length, None if fault
-        :return: int | None
-        """
-        if not self.exist:
-            return None
-
-        # try if given file is valid json format, return None otherwise
-        try:
-            with open(self.path) as f:
-                _json = json.load(f)
-        except json.decoder.JSONDecodeError:
-            return None
-
-        # try to establish length of the json if ValueError return None
-        try:
-            _length = len(_json)
-            return _length
-        except ValueError:
-            return None
-
-
 if __name__ == '__main__':
-    file_init = FileInitController()
+    fic = FileInitController()
